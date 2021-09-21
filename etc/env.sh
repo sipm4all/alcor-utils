340f20e595409510806701235a1ebd10b986a0e8
Roberto Preghenella <preghenella@bo.infn.it>
2021-09-21 16:25:34 +0200

    Update env script

--- etc/env.sh
+++ etc/env.sh
@@ -1,2 +1,14 @@
 export PATH=$PATH:/opt/cactus/bin/
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/cactus/lib/
+
+## environments for ALCOR DAQ
+export DRICH_OUT=/home/eic/DATA
+export ALCOR_DIR=/home/eic/alcor/alcor-utils
+export ALCOR_LIB=${ALCOR_DIR}/lib
+export ALCOR_CONF=${ALCOR_DIR}/conf
+export ALCOR_ETC=${ALCOR_DIR}/etc
+export PYTHONPATH=${ALCOR_LIB}
+export PCR_DIR=${ALCOR_CONF}/pcr
+export BCR_DIR=${ALCOR_CONF}/bcr
+
+alias alcgui="${ALCOR_LIB}/alcorGUI.sh"
