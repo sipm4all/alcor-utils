a95f020784fbee54cca710b32510ad25f15599d6
Roberto Preghenella <preghenella@bo.infn.it>
2021-10-09 14:45:52 +0200

    Channel mapping EO <--> DO

---
+++ readout/python/mapping.py
@@ -0,0 +1,5 @@
+# detector-oriented channel from electronics-oriented
+eo2do = [22, 20, 18, 16, 24, 26, 28, 30, 25, 27, 29, 31, 23, 21, 19, 17, 9, 11, 13, 15, 7, 5, 3, 1, 6, 4, 2, 0, 8, 10, 12, 14]
+
+# electronics-oriented channel from detector-oriented
+do2eo = [27, 23, 26, 22, 25, 21, 24, 20, 28, 16, 29, 17, 30, 18, 31, 19, 3, 15, 2, 14, 1, 13, 0, 12, 4, 8, 5, 9, 6, 10, 7, 11]
