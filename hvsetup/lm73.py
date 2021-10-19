09c6e292dd2ab3ade205fd038f6feefb6316fea8
Roberto Preghenella <preghenella@bo.infn.it>
2021-10-19 16:31:43 +0200

    Simple script to read LM73 temperature

---
+++ hvsetup/lm73.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+
+import serial
+import time
+import sys
+
+
+if (len(sys.argv)) is not 2:
+    print('usage: ./lm73.py [device]')
+    exit()
+
+ser = serial.Serial(sys.argv[1], baudrate=115200, timeout=0.1)
+data = 'L \r'
+ser.write(data)
+time.sleep(0.1)
+output = ser.readline()
+while output != '':
+    output = output.strip()
+    if output != '' and output != ':>':
+        print(output)
+    output = ser.readline()
+
+ser.close()
