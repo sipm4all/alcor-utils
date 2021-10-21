ae973b3dd03af2d840a69882d0c3f9378be1fd0f
Roberto Preghenella <preghenella@bo.infn.it>
2021-10-21 15:42:46 +0200

    Alcorinit update

--- control/alcorInit.sh
+++ control/alcorInit.sh
@@ -7,6 +7,10 @@ RDOUT_CONF=${ALCOR_CONF}/readout.conf
 CONN=${ALCOR_ETC}/connection2.xml
 runNr=$1
 DUMP_DIR=$2
+THRSCAN=false
+if [ "$3" = "true" ]; then
+    THRSCAN=true
+fi
 SWITCH="-s -i -m 0xffffffff -p 1"
 
 if [ $runNr -eq 0 ]; then
@@ -26,7 +30,11 @@ do
    ldec=$(printf "%d" $lane)
    if [ $ldec -ne 0 ]; then
        echo Programming $chip
-       ./alcorInit.py $CONN kc705 -c $chip $SWITCH --eccr $eccr --bcrfile ${BCR_DIR}/$bcr.bcr --pcrfile ${PCR_DIR}/$pcr.pcr
+       if [ "$THRSCAN" = true ]; then
+	   ./alcorInit.py $CONN kc705 -c $chip $SWITCH --eccr 0xb81b --bcrfile ${BCR_DIR}/$bcr.bcr --pcrfile ${PCR_DIR}/$pcr.pcr &
+       else
+	   ./alcorInit.py $CONN kc705 -c $chip $SWITCH --eccr $eccr --bcrfile ${BCR_DIR}/$bcr.bcr --pcrfile ${PCR_DIR}/$pcr.pcr
+       fi
        if [ $runNr -ne 0 ]; then
 	   cp ${BCR_DIR}/$bcr.bcr $DUMP_DIR/chip$chip.bcr
 	   cp ${PCR_DIR}/$pcr.pcr $DUMP_DIR/chip$chip.pcr
@@ -34,5 +42,10 @@ do
    fi
  fi
 done < $RDOUT_CONF | tee $DUMP_LOG
+
+if [ "$THRSCAN" = true ]; then
+    wait
+fi
+sleep 0.1
 
 cd $here
