3e34a388125003d34356f7278a32774cafe827bb
eic <eic@eicdesk01.bo.infn.it>
2022-05-13 20:49:48 +0200

    update tti directory

---
+++ tti/plh250.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python3
+
+import sys
+import serial
+
+ser = serial.Serial('/dev/PLH250', 9600, timeout=1)
+
+def send(cmd):
+    cmd += '\n'
+    ser.write(cmd.encode())
+
+def recv():
+    data = ser.readline().decode().strip()
+    return data
+
+def ask(cmd):
+    send(cmd)
+    return recv()
+
+if len(sys.argv) < 2:
+    print('usage: ./memmert_cmd.py [command]')
+    sys.exit(0)
+
+command = sys.argv[1]
+
+if command[-1:] == '?':
+    print('--- ask instrument:', command)
+    data = ask(command)
+    print('--- received data:', data)
+else:
+    print(' --- send instrument:', command)
+    send(command)
+
+ser.close()
