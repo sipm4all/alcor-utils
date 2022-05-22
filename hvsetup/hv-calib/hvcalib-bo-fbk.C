dce90c6ef28b83139e122b9a039125bc4a3b7ac3
eic <eic@eicdesk01.bo.infn.it>
2022-05-22 11:04:43 +0200

    Incremental update preparing for TIFPA-2022

--- hvsetup/hv-calib/hvcalib-bo-fbk.C
+++ hvsetup/hv-calib/hvcalib-bo-fbk.C
@@ -16,16 +16,21 @@ float hv[] = {
 };
 
 void
-hvcalib(double target = 10.)
+hvcalib(double target = 10., bool reverse = false)
 {
   auto n = sizeof(dac) / 4;
-  auto g = new TGraph(n, hv, dac);
+  auto g = reverse ? new TGraph(n, dac, hv) : new TGraph(n, hv, dac);
   auto f = (TF1 *)gROOT->GetFunction("pol1");
   g->Fit(f);
   g->Draw("ap*");
   f->Draw("same");
 
-  std::cout << " --- target voltage: " << target << std::endl;
-  std::cout << " --- DAC setting:    " << std::round(f->Eval(target)) << std::endl;
-}
+  if (reverse) {
+    std::cout << " --- DAC setting: " << target << std::endl;
+    std::cout << " --- supplied voltage:    " << f->Eval(target) << std::endl;
+  } else {
+    std::cout << " --- target voltage: " << target << std::endl;
+    std::cout << " --- DAC setting:    " << std::round(f->Eval(target)) << std::endl;
+  }
+  }
 
--- hvsetup/hvsetup.sh
+++ hvsetup/hvsetup.sh
@@ -1,14 +1,29 @@
 #! /usr/bin/env bash
 
-stty speed 115200 -F $1
-echo " --- setting HV on $1 from $2"
+SLEEPS="0.5"
+DEVICE=$1
+
+stty speed 115200 -F $DEVICE
+echo " --- setting HV on $DEVICE from $2"
 read -p " are you sure? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-	cat $2 | while read line; do
-		echo $line
-		(echo -e $line > $1);
-		sleep 1; 
-	done
+
+    echo -e "P \n\r" > $DEVICE
+    sleep $SLEEPS
+    cat $2 | while read line; do
+	echo $line
+	echo -e "$line \r" > $DEVICE
+	sleep $SLEEPS; 
+    done
+
 fi
+
+### read back DAC values
+echo " --- $DEVICE reading "
+(cat $DEVICE | grep DAC12) &
+pid=$!
+echo -e "R \n\r" > $DEVICE && sleep $SLEEPS
+killall cat
+echo
