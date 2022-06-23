3aec5204e47356a34b4eb91ca289c64b6fef7b9a
eic <eic@eicdesk02.bo.infn.it>
2022-06-23 15:14:19 +0200

    updates from eicdesk02

--- firmware/check_kc705.sh
+++ firmware/check_kc705.sh
@@ -4,4 +4,9 @@ source /tools/Xilinx/Vivado_Lab/2021.2/.settings64-Vivado_Lab.sh
 source /home/eic/alcor/alcor-utils/etc/env.sh
 
 FW="new"
-ping -c1 10.0.8.15 || /home/eic/alcor/alcor-utils/firmware/program.sh $FW 210203A62F62A true &> /tmp/firmware_program.log
+TARGET=210203A62F62A
+if [ ! -x $KC705_TARGET ]; then
+    TARGET=$KC705_TARGET
+fi
+
+ping -c1 10.0.8.15 || /home/eic/alcor/alcor-utils/firmware/program.sh $FW $TARGET true &> /tmp/firmware_program.log
