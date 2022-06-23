0316229a33a74974e7c7e135b0f6979857ac5338
eic <eic@eicdesk02.bo.infn.it>
2022-06-23 15:12:19 +0200

    allow to write alcor registers

--- readout/src/alcor_register.cc
+++ readout/src/alcor_register.cc
@@ -13,6 +13,7 @@
 
 struct program_options_t {
   std::string connection_filename, device_id, write_value;
+  int bcr, pcr, pixel, column;
   int chip;
 };
 
@@ -30,6 +31,10 @@ process_program_options(int argc, char *argv[], program_options_t &opt)
       ("connection"       , po::value<std::string>(&opt.connection_filename)->required(), "IPbus XML connection file")
       ("device"           , po::value<std::string>(&opt.device_id)->default_value("kc705"), "Device ID")
       ("chip"             , po::value<int>(&opt.chip)->required(), "ALCOR chip number")
+      ("bcr"              , po::value<int>(&opt.bcr)->default_value(-1), "ALCOR BCR register")
+      ("pcr"              , po::value<int>(&opt.pcr)->default_value(-1), "ALCOR PCR register")
+      ("pixel"            , po::value<int>(&opt.pixel)->default_value(0), "ALCOR PCR pixel register")
+      ("column"           , po::value<int>(&opt.column)->default_value(0), "ALCOR PCR column register")
       ("write"            , po::value<std::string>(&opt.write_value), "Write a value before reading")
       ;
     
@@ -55,14 +60,24 @@ int main(int argc, char *argv[])
   program_options_t opt;
   process_program_options(argc, argv, opt);
 
+  uhal::disableLogging();
   uhal::ConnectionManager connection_manager("file://" + opt.connection_filename);
   uhal::HwInterface hardware = connection_manager.getDevice(opt.device_id);
 
   alcor::alcor_t alcor;
   alcor.init(hardware, opt.chip);
 
-  alcor.spi.write(ECCR(0), 0xb81b);
-  printf("ECCR(0) = 0x%04x \n", alcor.spi.read(ECCR(0)));
+  if (!opt.write_value.empty()) {
+    unsigned int value;
+    std::stringstream ss;
+    ss << std::hex << opt.write_value;
+    ss >> value;
+    if (opt.bcr != -1) alcor.spi.write(BCR(opt.bcr), value);
+    if (opt.pcr != -1) alcor.spi.write(PCR(opt.pcr, opt.pixel, opt.column), value);
+  }
+  if (opt.bcr != -1) printf("BCR(%d) = 0x%04x \n", opt.bcr, alcor.spi.read(BCR(opt.bcr)));
+  if (opt.pcr != -1) printf("PCR(%d,%d,%d) = 0x%04x \n", opt.pcr, opt.pixel, opt.column, alcor.spi.read(PCR(opt.pcr, opt.pixel, opt.column)));
 
   return 0;
+
 }
