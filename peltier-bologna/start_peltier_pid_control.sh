573463bca57c2f41a56d7417cc8caf1afc93fc09
eic <eic@eicdesk02@bo.infn.it>
2023-04-02 02:46:53 +0200

    updates as code is found today

--- peltier-bologna/start_peltier_pid_control.sh
+++ peltier-bologna/start_peltier_pid_control.sh
@@ -1,10 +1,11 @@
 #! /usr/bin/env bash
 
 COMMAND="/home/eic/alcor/alcor-utils/peltier-bologna/peltier_pid_control.py"
+COMMAND="/home/eic/alcor/alcor-utils/peltier-bologna/peltier_pid_control_airbox.py" ### R+TEMP
 PIDLOCK="/tmp/peltier_pid_control.pid"
 
 ### check arduino server is running, otherwise start it
-if [ $(ps -ef | grep peltier_pid_control.py | grep python3 | wc -l ) = "0" ]; then
+if [ $(ps -ef | grep peltier_pid_control | grep python3 | wc -l ) = "0" ]; then
     echo " --- peltier_pid_control not running: starting it "
     $COMMAND &> /tmp/peltier_pid_control.log &
     echo $! > $PIDLOCK
