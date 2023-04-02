4f946fc49de95047b9e044d9b89b15dc0b277317
eic <eic@eicdesk02@bo.infn.it>
2023-04-02 02:47:17 +0200

    updates as code is found today

--- flask/mini.py
+++ flask/mini.py
@@ -2,6 +2,7 @@
 
 from flask import Flask, render_template, request, redirect
 import os
+import time
 
 app = Flask(__name__)
 
@@ -29,19 +30,80 @@ def masterlogic(masterlogic_name=None, masterlogic_number=None):
     if request.method == 'POST':
         print(request.form)
 
+        trim_channel = request.form.get('trim_channel')
+        trim_value = request.form.get('trim_value')
+        trim_set = request.form.get('trim_set')
+        if trim_set == 'trim':
+            if isfloat(trim_channel) and isfloat(trim_value):
+                print('/au/masterlogic/trim {} {} {} &'.format(masterlogic_number, trim_channel, trim_value))
+                themessage = 'setting trim value to {}: {}'.format(trim_channel, trim_value)
+                os.system('/au/masterlogic/masterlogic_trim.sh {} {} {} &'.format(masterlogic_number, trim_channel, trim_value))
+        
         voltage = request.form.get('voltage')
         voltage_set = request.form.get('voltage_set')
         if voltage_set == 'zero':
             themessage = 'setting voltage to ZERO'
             os.system('/au/masterlogic/zero {} &'.format(masterlogic_number))
+        elif voltage_set == 'tuned':
+            vdiff = 0.
+            if isfloat(voltage):
+                vdiff = voltage
+            themessage = 'setting voltage to TUNED values: with {} V difference'.format(vdiff)
+            os.system('/au/masterlogic/conf/create_tuned.sh {}'.format(vdiff))
+            os.system('/au/masterlogic/masterlogic_tuned.sh {} {} /au/masterlogic/conf/masterlogic.{}.dac12 /au/masterlogic/conf/masterlogic.{}.dac8 &'.format(masterlogic_number, masterlogic_name.lower(), masterlogic_name.lower(), masterlogic_name.lower()))
         elif isfloat(voltage):
             themessage = 'setting voltage to {}: {} V'.format(voltage_set, voltage)
             os.system('/au/masterlogic/voltage_set.sh {} {} {} {} &'.format(masterlogic_number, masterlogic_name, voltage, voltage_set))
-                
+
         return render_template('masterlogic.html', message=themessage, name=masterlogic_name, number=masterlogic_number)
 
     # otherwise handle the GET request
     return render_template('masterlogic.html', message=themessage, name=masterlogic_name, number=masterlogic_number)
+
+@app.route('/readout', methods=['GET', 'POST'])
+def readout():
+    themessage = 'ready to accept commands'
+    # handle the POST request
+    if request.method == 'POST':
+        print(request.form)                
+
+        readout_proc = request.form.get('readout_proc')
+        readout_run = request.form.get('readout_run')
+        readout_spill = request.form.get('readout_spill')
+
+        if readout_proc == 'update':
+            os.system('/au/measure/readout-box/readout_system_monitor.sh &')
+            themessage = 'readout monitor update requested'
+        elif readout_proc == 'start':
+            os.system('/au/readout/scripts/start-readout-processes.sh &')            
+            themessage = 'starting readout processes'
+        elif readout_proc == 'kill':
+            themessage = 'killing readout processes'
+        elif readout_proc == 'reset':
+            os.system('/au/readout/scripts/mode_reset.sh &')
+            themessage = 'FPGA mode reset sent'
+
+        if readout_run == 'start':
+            os.system('/au/readout/python/interprocess.py 0x3f9 &')
+            themessage = 'sent interprocess command: start run'
+        elif readout_run == 'stop':
+            os.system('/au/readout/python/interprocess.py 0x0 &')
+            themessage = 'sent interprocess command: stop run'
+            
+        if readout_spill == 'start':
+            os.system('/au/readout/python/interprocess.py 0x3fd &')
+            themessage = 'sent interprocess command: start spill'
+        elif readout_spill == 'stop':
+            os.system('/au/readout/python/interprocess.py 0x3f9 &')
+            themessage = 'sent interprocess command: stop spill'
+            
+        time.sleep(1)
+        os.system('/au/measure/readout-box/readout_system_monitor.sh &')
+            
+        return render_template('readout.html', message=themessage)
+    
+    # otherwise handle the GET request
+    return render_template('readout.html', message=themessage)
 
 @app.route('/', methods=['GET', 'POST'])
 def form_example():
