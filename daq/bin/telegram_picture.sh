36d5e32903c1d9512fb6ab7937fcb24f9f48d14b
eicserv01 <eic@eicserv01>
2024-04-25 22:34:14 +0200

    switch to 2024 test beam telegram channel

--- daq/bin/telegram_picture.sh
+++ daq/bin/telegram_picture.sh
@@ -7,9 +7,9 @@ fi
 picture=$1
 caption="$(hostname) -- $2"
 
-
-#chatid=-1001866731573 ### group 
-#chatid=625973013 ### roberto
-chatid=-1001983362927
+chatid=-1001866731573 ### group 
+chatid=625973013 ### roberto
+chatid=-1001983362927 ### 2023 channel
+chatid=-1002098739640 ### 2024 channel
 
 timeout 10 curl -X POST -H "Content-Type:multipart/form-data" -F chat_id=$chatid -F photo=@$picture -F caption="$caption" "https://api.telegram.org/bot6485476926:AAEqeA-ynsn4fzHFzWQdPQs2LgnwgwXOSpk/sendPhoto" &> /dev/null
