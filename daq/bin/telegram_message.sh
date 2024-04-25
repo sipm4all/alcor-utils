5b8ba0e3b0b6563dc4ef53f769d730536e94e4a0
eicserv01 <eic@eicserv01>
2024-04-25 22:34:14 +0200

    switch to 2024 test beam telegram channel

--- daq/bin/telegram_message.sh
+++ daq/bin/telegram_message.sh
@@ -6,9 +6,10 @@ if [ "$#" -ne 1 ]; then
 fi
 message="$(hostname) -- $1"
 
-#chatid=-1001866731573 ### group 
-#chatid=625973013 ### roberto
-chatid=-1001983362927
+chatid=-1001866731573 ### group 
+chatid=625973013 ### roberto
+chatid=-1001983362927 ### 2023 channel
+chatid=-1002098739640 ### 2024 channel
 
 timeout 10 curl -X POST -H "Content-Type:multipart/form-data" -F chat_id=$chatid -F text="$message" "https://api.telegram.org/bot6485476926:AAEqeA-ynsn4fzHFzWQdPQs2LgnwgwXOSpk/sendMessage" &> /dev/null
 
