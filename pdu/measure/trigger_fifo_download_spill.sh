29d19d2926ede38a61512171f1d3bbabeae2f89f
eicserv01 <eic@eicserv01>
2024-05-04 15:13:12 +0200

    trigger FIFO download scripts

---
+++ pdu/measure/trigger_fifo_download_spill.sh
@@ -0,0 +1,54 @@
+#! /usr/bin/env bash
+
+if [ "$#" -ne 2 ]; then
+    echo " usage: $0 [device] [sleep] "
+    exit 1
+fi
+device=$1
+sleep=$2
+
+reg="/au/readout/bin/register --connection /etc/drich/drich_ipbus_connections.xml --device ${device}"
+blo="/au/readout/bin/block --connection /etc/drich/drich_ipbus_connections.xml --device ${device}"
+
+main()
+{
+
+    ### run sequence
+    mode 0x0       # run OFF
+    mode 0x1       # run ON
+    reset          # reset FIFO
+    mode 0x5       # spill ON
+    echo " --- sleep $sleep seconds "
+    sleep $sleep   # sleep
+    mode 0x1       # spill OFF
+    mode 0x0       # run OFF
+
+    ### download data
+    download /tmp/data.dat
+}
+
+download()
+{
+    rm -f $1
+    while true; do
+	### read FIFO occupancy
+	nwords=$(${reg} --node trigger_info.fifo_occupancy | awk {'print $3'})
+	nwords=$(($nwords & 0xFFFF))
+	echo " --- there are $nwords words in the FIFO "
+	[[ $nwords == "0" ]] && break;
+	${blo} --node trigger_info.fifo_data --size ${nwords} >> $1
+    done
+    echo " --- downloaded data: $1 "
+}
+
+mode()
+{
+    ${reg} --node regfile.mode --write $1
+}
+
+reset()
+{
+    ${reg} --node trigger_info.fifo_reset --write 1
+}
+
+main
