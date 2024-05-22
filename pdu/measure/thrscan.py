4a8fee60cd7382da395b19ca3e8de96397a38fd3
eicdesk03 <eic@eicdesk03>
2024-05-22 22:10:22 +0200

    improved thrscan

--- pdu/measure/thrscan.py
+++ pdu/measure/thrscan.py
@@ -1,6 +1,11 @@
 #! /usr/bin/env python
 
+import sys
 import matplotlib.pyplot as plt
+
+title=''
+if len(sys.argv) > 0:
+    title = sys.argv[1]
 
 # Open the data file for reading
 with open('/tmp/thrscan.dat', 'r') as file:
@@ -11,9 +16,9 @@ with open('/tmp/thrscan.dat', 'r') as file:
 plt.semilogy(x_values, y_values, marker='o', linestyle='-')
 
 # Add labels and a title
-plt.xlabel('X-axis Label')
-plt.ylabel('Y-axis Label')
-plt.title('Your Graph Title')
+plt.xlabel('threshold (au)')
+plt.ylabel('count rate (Hz)')
+plt.title(title)
 
 # Display the plot (or save it to a file using plt.savefig('output.png'))
 plt.savefig('/tmp/thrscan.png')
--- pdu/measure/thrscan.sh
+++ pdu/measure/thrscan.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env bash
 
-if [ "$#" -ne 5 ]; then
-    echo " usage: $0 [device] [chip] [channel] [vth] [offset] "
+if [ "$#" -ne 6 ]; then
+    echo " usage: $0 [device] [chip] [channel] [vth] [offset] [timer] "
     exit 1
 fi
 device=$1
@@ -9,10 +9,13 @@ chip=$2
 channel=$3
 vth=$4
 offset=$5
+timer=$6
 
 for threshold in {0..63}; do
-    /au/pdu/measure/rate.sh $device $chip $channel --range 2 --vth $vth --offset $offset --threshold $threshold | awk {'print $3, $15'} 
+    /au/pdu/measure/rate.sh $device $chip $channel --min_timer 31250 --min_counts 1000 --max_timer $timer --range 2 --vth $vth --offset $offset --threshold $threshold | awk {'print $3, $15'} 
 done | tee /tmp/thrscan.dat
 
-/au/pdu/measure/thrscan.py
+/au/pdu/measure/thrscan.py "${device} chip-${chip} chan-${channel} (vth=${vth}, off=${offset})"
 cp /tmp/thrscan.png ~/DATA/.
+
+/home/eic/bin/telegram_picture.sh /tmp/thrscan.png "thrscan"
